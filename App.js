5bc8ff9159887d1c771ec8b15f86f3815dd2b9ce
Nathan <natan.kroizer@go-arc.com>
2021-10-05 09:37:14 +0300

    Assets

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PokedexScreen from './components/PokedexMain'
+import RegionPokedex from './components/RegionPokedex'
+import PokemonDetails from './components/PokemonDetails'
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Pokedex"
+        onPress={() => navigation.navigate('Pokedex')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Pokedex" component={PokedexScreen} />
+        <Stack.Screen name="Kanto" component={RegionPokedex} />
+        <Stack.Screen name="Jhoto" component={RegionPokedex} />
+        <Stack.Screen name="Hoenn" component={RegionPokedex} />
+        <Stack.Screen name="Sinnoh" component={RegionPokedex} />
+        <Stack.Screen name="Unova" component={RegionPokedex} />
+        <Stack.Screen name="Kalos" component={RegionPokedex} />
+        <Stack.Screen name="Alola" component={RegionPokedex} />
+        <Stack.Screen name="Galar" component={RegionPokedex} />
+        <Stack.Screen name="Other" component={RegionPokedex} />
+        <Stack.Screen name="PokemonDetails" component={PokemonDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
